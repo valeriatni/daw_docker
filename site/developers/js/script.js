c0a105c89532ce1a103a7ead8e7c556c5ba2a214
Valeria TN <vticonan@unsa.edu.pe>
2026-04-16 03:12:27 -0500

    Agregando script

---
+++ site/developers/js/script.js
@@ -0,0 +1,6 @@
+function cambiarColor() {
+    document.body.style.backgroundColor = "lightblue";
+
+    document.getElementById("mensaje").innerText =
+        "Formulario enviado correctamente";
+}
